a53c886423ddb93af637200c664e18acff49b972
Vincent Chau <vichau@ucdavis.edu>
2016-06-28 11:31:41 -0700

    generic linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,9 +1,73 @@
 
 public class LinkedList {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("sdaf");
+	private Node head;
+	private int count;
+	
+	public LinkedList(){
+		head = new Node(null);
+		count = 0;
+	}
+	public void add(Integer data){
+		Node temp = new Node(data);
+		Node current = head;
+		while(current.getNext() != null){
+			current = current.getNext();
+		}
+		current.setNext(temp);
+		count++;
+	}
+	public void add(Integer data, int index){
+		Node temp = new Node(data);
+		Node current = head;
+		for(int i = 1; i < index && current.getNext() != null; i++){
+			current = current.getNext();
+		}
+		temp.setNext(current.getNext());
+		current.setNext(temp);
+		count++;
+	}
+	public boolean remove(int index){
+		if(index < 1 || index > size()){
+			return false;
+		}
+		Node current = head;
+		for(int i = 0; i < index; i++){
+			if(current.getNext() == null)
+				return false;
+			current = current.getNext();
+		}
+		current.setNext(current.getNext().getNext());
+		count--;
+		return true;
+	}
+	public int size(){
+		return count;
 	}
 
+	private class Node{
+		Node next;
+		Integer data;
+		public Node(Integer data){
+			next = null;
+			this.data = data;
+		}
+		public Integer getData(){
+			return data;
+		}
+		public void setData(Integer _number){
+			data = _number;
+		}
+		public Node getNext(){
+			return next;
+		}
+		public void setNext(Node _next){
+			next = _next;
+		}
+	}
+	public static void main(String[] args) {
+
+		
+	}
+	
 }
+
